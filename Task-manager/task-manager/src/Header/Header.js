6fc6fe32b74a97cf0b6be5b9fa24caec3f12a619
sebciulina <sgransicki@gmail.com>
2022-11-28 14:25:02 +0100

    header and logo

--- Task-manager/task-manager/src/Header/Header.js
+++ Task-manager/task-manager/src/Header/Header.js
@@ -8,7 +8,14 @@ function Header() {
         <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img 
+                src="/logo.png"
+                width="300"
+                height="300"
+                alt="TM"
+            />
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
