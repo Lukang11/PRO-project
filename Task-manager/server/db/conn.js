fba1930e4afaf6ed1feaf957239ce8c83f6d5c46
MaciejDubowik <maciejxpoland@gmail.com>
2023-01-15 13:26:51 +0100

    MongoDB Atlas connection fix

--- Task-manager/server/db/conn.js
+++ Task-manager/server/db/conn.js
@@ -1,26 +1,24 @@
 const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
 
-var _db;
+// Connection URI
+const uri =
+    "mongodb+srv://admin:admin@task-manager.u7qr6rf.mongodb.net/?retryWrites=true&w=majority";
 
-module.exports = {
-    connectToServer: function (callback) {
-        client.connect(function (err, db) {
-            // Verify we got a good "db" object
-            if (db)
-            {
-                _db = db.db("employees");
-                console.log("Successfully connected to MongoDB.");
-            }
-            return callback(err);
-        });
-    },
+// Create a new MongoClient
+const client = new MongoClient(uri);
 
-    getDb: function () {
-        return _db;
-    },
-};
+// conn.js
+async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Establish and verify connection
+        await client.db("Task-Manager").command({ ping: 1 });
+        console.log("Connected successfully to server");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+module.exports = { run }
