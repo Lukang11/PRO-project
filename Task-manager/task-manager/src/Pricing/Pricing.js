73db5908f3c3ebd0f270b1772e78e951822d51b4
Filiposki54 <filip.kierczynski@gmail.com>
2023-01-23 20:05:10 +0100

    fixed pricing
    fixed backaground colour

--- Task-manager/task-manager/src/Pricing/Pricing.js
+++ Task-manager/task-manager/src/Pricing/Pricing.js
@@ -7,8 +7,10 @@ export function Pricing() {
         <h2>Plan standardowy</h2>
         <p className='price'>9.99$/miesiąc</p>
         <ul>
-          <li>Możliwość dodania nieograniczonej liczby użytkowników</li>
-          <li>500MB miejsca na bazie danych</li>
+          <li className='pricing-li'>
+            Możliwość dodania nieograniczonej liczby użytkowników
+          </li>
+          <li className='pricing-li'>500MB miejsca na bazie danych</li>
         </ul>
         <button>Wybierz plan</button>
       </div>
@@ -17,8 +19,10 @@ export function Pricing() {
         <h2>Plan rozszerzony</h2>
         <p className='price'>19.99$/miesiąc</p>
         <ul>
-          <li>Możliwość dodania nieograniczonej liczby użytkowników</li>
-          <li>2GB miejsca na bazie danych</li>
+          <li className='pricing-li'>
+            Możliwość dodania nieograniczonej liczby użytkowników
+          </li>
+          <li className='pricing-li'>2GB miejsca na bazie danych</li>
         </ul>
         <button>Wybierz plan</button>
       </div>
@@ -27,9 +31,13 @@ export function Pricing() {
         <h2>Plan premium</h2>
         <p className='price'>29.99$/miesiąc</p>
         <ul>
-          <li>Możliwość dodania nieograniczonej liczby użytkowników</li>
-          <li>10GB miejsca na bazie danych</li>
-          <li>Dostępność dedykowanego opiekuna technicznego</li>
+          <li className='pricing-li'>
+            Możliwość dodania nieograniczonej liczby użytkowników
+          </li>
+          <li className='pricing-li'>10GB miejsca na bazie danych</li>
+          <li className='pricing-li'>
+            Dostępność dedykowanego opiekuna technicznego
+          </li>
         </ul>
         <button>Wybierz plan</button>
       </div>
